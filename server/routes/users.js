a7b2ff1c2eb4284a674efa9bebf4c01ebe863379
Joel <joel.m.maenpaa@gmail.com>
2020-02-06 17:01:14 +0200

    register route mods, login route made

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,22 +1,69 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+// private key
+require('dotenv').config()
+const privateKey = process.env.PRIVATE_KEY
 
 // user model
 const User = require('../models/Users')
 
-router.post('/users/add', (req, res) => {
+router.post('/register', (req, res) => {
+    // find if username already exists
     User.findOne({ username: req.body.username }).then(user => {
         if (user) {
-            return res.status(400).json({ status: "Username already exists" })
+            return res.status(400).json({ 
+                status: "Username already exists",
+                success: false
+            })
         } else {
             const newUser = new User({
                 username: req.body.username,
                 password: req.body.password
             })
 
+            // save user in db
             newUser.save()
-                .then(user => res.json(user))
+                .then(user => res.status(200).json(user))
                 .catch(err => console.log(err))
         }
+    })
+})
+
+router.post('/login', (req, res) => {
+    // find user by username and check if it doesn't exist
+    User.findOne({ username: req.body.username }).then(user => {
+        if (!user) {
+            return res.status(400).json({ 
+                status: "User not found",
+                success: false
+            })
+        }
+
+        // compare the passwords
+        user.comparePasswords(req.body.password, function(err, isMatch) {
+            if (err) throw err
+
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.username
+                }
+
+                // token creation
+                jwt.sign(payload, privateKey, { expiresIn: 30 }, (err, token) => {
+                    res.status(200).json({
+                        success: true,
+                        token: token
+                    })
+                })
+            } else {
+                return res.status(400).json({
+                    status: "Password incorrect",
+                    success: false
+                })
+            }
+        })
     })
 })
 
