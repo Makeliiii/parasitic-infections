0bb859c2e18e89c27f7866d9221aaf29403ec04d
Joel <joel.m.maenpaa@gmail.com>
2020-02-06 14:04:32 +0200

    index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const port = 3000
+
+// import routes
+const users = require('./routes/users')
+
+// db uri
+require('dotenv').config()
+const db = process.env.DB_CONNECTION
+
+// body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// cors
+app.use(cors())
+
+// routes
+app.use('/api', users)
+
+// db connection
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true } , () => console.log(`DB connection established`))
+
+// start server
+app.listen(port, () => console.log(`App listening on port ${port}`))
