d16663ec6fd4e6a8dafa7e3c2db2a74e85140bd7
Joel <joel.m.maenpaa@gmail.com>
2020-03-13 12:17:00 +0200

    fetch items and display them

--- client/components/Views/Main.js
+++ client/components/Views/Main.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react'
-import { Text, View, Image, Button } from 'react-native';
+import { Text, View, ScrollView, Image, Button } from 'react-native'
+
+import Post from './Post/Post'
 
 class Main extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            items: []
         }
+    }
+
+    componentDidMount() {
+        fetch('http://54.174.235.204/api/items/get')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({ items: data.items })
+                console.log(this.state.items)
+            })
     }
 
     render() {
         return (
             <View style={{ justifyContent: "center", alignItems: "center" }}>
+                <ScrollView style={{ width: '100%' }}>
                 <Image 
-                    style={{ width: 256, height: 256, marginTop: '25%' }}
+                    style={{ width: 128, height: 128, marginBottom: '10%', marginTop: '25%', alignSelf: 'center' }}
                     source={{ uri: 'https://agony.s-ul.eu/6RLsganS' }}
                 />
+                <Post items={ this.state.items } />
                 <Button
                     title='Logout'
                     onPress={ this.props.logout }
                 />
+                </ScrollView>
             </View>
         )
     }
